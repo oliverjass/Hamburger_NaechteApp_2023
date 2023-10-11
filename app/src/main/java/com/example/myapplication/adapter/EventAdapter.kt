a0d75cd349fb2b79b6641c45b1d9eb46f0d0c316
oliverjass <oliver.jass.1997@gmail.com>
2023-10-11 17:28:05 +0200

    Version 1.3

---
+++ app/src/main/java/com/example/myapplication/adapter/EventAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.myapplication.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.myapplication.R
+import com.example.myapplication.databinding.EventItemBinding
+import com.example.myapplication.model.EventModel
+
+class EventAdapter (private val eventModel: List<EventModel>
+) : RecyclerView.Adapter<EventAdapter.EventViewHolder>() {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.event_item,parent,false)
+        return EventViewHolder(view)
+    }
+
+
+    override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
+        holder.binding.apply {
+            eventPosterIMG.load(eventModel[position].imageUrl)
+        }
+    }
+
+
+    inner class EventViewHolder(itemView:View) : RecyclerView.ViewHolder(itemView) {
+        val binding = EventItemBinding.bind(itemView)
+
+    }
+
+
+    override fun getItemCount() = eventModel.size
+}
+
+
+
+
---
+++ app/src/main/java/com/example/myapplication/ui/EventFragment.kt
@@ -0,0 +1,31 @@
+package com.example.myapplication.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myapplication.MainActivity
+import com.example.myapplication.databinding.FragmentEventBinding
+
+class EventFragment : Fragment() {
+
+    private lateinit var binding: FragmentEventBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentEventBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mainActivityevents = (activity as MainActivity).dataDir
+
+        binding.MainRV.hasFixedSize()
+
+    }
+}
