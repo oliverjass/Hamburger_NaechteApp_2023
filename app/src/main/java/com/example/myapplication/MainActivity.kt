bbbb9fa1566b63110642beee1e1c9b7a8c7d7aaf
oliverjass <oliver.jass.1997@gmail.com>
2023-10-11 13:37:41 +0200

    Version 1.0

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.myapplication.R
 import com.example.myapplication.adapter.MainAdapter
 import com.example.myapplication.databinding.ActivityMainBinding
@@ -10,25 +15,31 @@ import com.example.myapplication.utils.SampleData
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
 
+        val navHostFragment: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
 
-        binding.apply {
-            MainRV.adapter=MainAdapter(SampleData.collections)
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            NavigationUI.onNavDestinationSelected(item, navController)
 
+            navController.popBackStack(item.itemId, false)
+            return@setOnItemSelectedListener true
         }
-
-
-
 
     }
 
-
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
 
 
 }
